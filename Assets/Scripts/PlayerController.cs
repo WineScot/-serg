8a58f00ff723e3ac468b9b65296fcafa9a8b6e8b
ozel981 <ozel981@interia.pl>
2018-06-22 23:11:18 +0200

    initialcommit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        /*float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal,moveVertical);
+        rb2d.AddForce(movement);*/
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector2 movement = new Vector2(0,80000);
+            rb2d.AddForce(movement);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Vector2 movement = new Vector2(0, -10000);
+            rb2d.AddForce(movement);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector2 movement = new Vector2(10000, 0);
+            rb2d.AddForce(movement);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector2 movement = new Vector2(-10000, 0);
+            rb2d.AddForce(movement);
+        }
+       
+    }
+}
