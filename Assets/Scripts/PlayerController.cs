63f6b66f62d9d66aa1113b65e2415e6e5a636e7d
Maxization <damian-opoka@wp.pl>
2018-06-23 18:50:01 +0200

    Poprawki w ruchu

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,38 +5,34 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb2d;
+    private Animator anim;
+
+    public float moveSpeed=10;
+    public float jumpHeight=60;
 
     void Start()
     {
+        anim = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        /*float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        Vector2 movement = new Vector2(moveHorizontal,moveVertical);
-        rb2d.AddForce(movement);*/
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Vector2 movement = new Vector2(0,80000);
-            rb2d.AddForce(movement);
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            Vector2 movement = new Vector2(0, -10000);
-            rb2d.AddForce(movement);
+            Vector2 movement = new Vector2(rb2d.velocity.x, jumpHeight);
+            rb2d.velocity = movement;
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            Vector2 movement = new Vector2(10000, 0);
-            rb2d.AddForce(movement);
+            Vector2 movement = new Vector2(moveSpeed, rb2d.velocity.y);
+            rb2d.velocity = movement;
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            Vector2 movement = new Vector2(-10000, 0);
-            rb2d.AddForce(movement);
+            Vector2 movement = new Vector2(-moveSpeed, rb2d.velocity.y);
+            rb2d.velocity = movement;
         }
-       
+        
     }
 }
