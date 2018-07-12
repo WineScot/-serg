b1f1f645f3f453b32f0dff89ced3b18c051468de
jaskier234 <jaskier234@gmail.com>
2018-07-12 10:30:13 +0200

    Część zmian Wojtka
    (reszty nie udało mi się przenieść)

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -6,6 +6,8 @@ public class WeaponController : MonoBehaviour {
 
     private GameObject sword1;
     private GameObject sword2;
+    private GameObject hand;
+    private Animator anim;
 
     bool GetDirectionRight() // dircetionRight from PlayerController script
     {
@@ -17,6 +19,8 @@ public class WeaponController : MonoBehaviour {
     {
         sword1 = GameObject.FindGameObjectWithTag("Sword1");
         sword2 = GameObject.FindGameObjectWithTag("Sword2");
+        hand = GameObject.FindGameObjectWithTag("Hand");
+        anim = hand.GetComponent<Animator>();
     }
 	
 	// Update is called once per frame
@@ -26,11 +30,14 @@ public class WeaponController : MonoBehaviour {
         {
             if (GetDirectionRight())
             {
+
                 sword2.gameObject.GetComponent<WeaponAttack>().activeAttackArea = true;
+                anim.SetTrigger("playerRightAttack");
             }
             else
             {
                 sword1.gameObject.GetComponent<WeaponAttack>().activeAttackArea = true;
+                anim.SetTrigger("playerLeftAttack");
             }
 
         }
